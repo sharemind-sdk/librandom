720e9854b8cf772090f7cd3dc38f7e75ac2d0000
Dan Bogdanov <dan.bogdanov@cyber.ee>
2009-06-14 13:43:20 +0000

    Seed the RNG from a stronger source.
    git-svn-id: https://research.cyber.ee/projects/sharemind/svnroot/trunk@507 10f935d1-c939-0410-8f69-b94c1d3c5283

--- src/Random.h
+++ src/Random.h
@@ -14,33 +14,63 @@
 #include "boost/thread/mutex.hpp"
 
 /**
- A random generator. Relies on the ANSI X9.17 Appendix C compliant generator from CryptoPP library.
+ A pseudorandomness generator. Relies on the ANSI X9.17 Appendix C 
+ compliant generator from CryptoPP library.
 
- \note The methods are not static so you must make an instance of it to use it.
+ \note The generator is to be used with instances created. 
+ The global generator is used to reinforce the seeds of the
+ individual instances.
+ 
+ \note The generator is not locked, so do not use one instance from several threads.
+ 
+ \note Sometime in the future we might implement something like this:
+ \code
+ local_thread:
+ get_rnd {
+	64bit local_index
+	64bit thread_index
+	return get_AES_block(++local_index,thread_index) 
+ }
+
+ rnd_thread
+
+ initialise_rnd()
+
+ get_AES_block(local_index,tread_index) {
+	aes_block = combine (local_index,tread_index)
+	return AES(key,aes_block)
+ }
+ \endcode
 */
 class Random {
 
 public:
+
 	/**
-	 Initializes the random generator
-	*/
+	 The constructor is private in a singleton
+	 */
 	Random();
-
-
+	
 	/**
-	 Cleans up after the generatorm if necessary
+	 The destructor of the randomness generator
 	*/
 	virtual ~Random();
 
 	/**
-	 Generates a random value
+	 Generates a single random value
 
-	 \returns a random value
+	 \returns a single 32-bit random integer
 	*/
 	val_t Generate();
 
 	/**
 	 Fills a vector with random values
+	 
+	 \param[in] vec the vector of values to fill
+	 \param[in] start the index to start filling from
+	 \param[in] end the index to fill to (not included)
+	 
+	 \return the number of values generated
 	*/
 	uint32 FillVector(val_vector_t& vec, uint32 start, uint32 end);
 
@@ -50,8 +80,21 @@ private:
 	 An instance of the CryptoPP generator.
 	*/
 	CryptoPP::DefaultAutoSeededRNG strongRNG;
+	
+	/**
+	 The global instance of this class
+	 */
+	static Random globalInstance;
 
-	boost::mutex theMutex;
+	/**
+	 The mutex guarding the global instance
+	 */
+	static boost::mutex globalInstanceMutex;
+	
+	/**
+	 True, if the global instance is initialized
+	 */
+	static bool globallyInitialized;
 
 };
 
