ba728b97caeb3c6d939bdea295c1628eb69f6f54
Jaak Randmets <jaak.randmets@cyber.ee>
2012-05-16 12:21:59 +0300

    Removed a lot of stupid from Random.h

--- src/Random.h
+++ src/Random.h
@@ -10,9 +10,7 @@
 #ifndef SHAREMINDCOMMON_RANDOM_H
 #define SHAREMINDCOMMON_RANDOM_H
 
-#include <boost/type_traits/is_same.hpp>
-#include <boost/type_traits/is_pointer.hpp>
-#include <vector>
+#include <cstddef>
 
 namespace sharemind {
 
@@ -35,111 +33,7 @@ public: /* Methods: */
      */
     virtual void fillBytes (void* memptr, size_t size) = 0;
 
-    template <typename T>
-    void fillValue (T& value) {
-        fillBytes (&value, sizeof (T));
-    }
-
-    template <typename T>
-    T generateValue () {
-        T result;
-        fillValue (result);
-        return result;
-    }
-
-    /**
-     * \brief Fills a subrange of vector with random values.
-     */
-    template <typename T>
-    void fillVector (std::vector<T>& vec, size_t begin, size_t end) {
-        if (begin < end && end <= vec.size ()) {
-            fillRange (vec.begin () + begin, vec.begin () + end);
-        }
-    }
-
-    /**
-     * \brief Fills entire vector with random values.
-     */
-    template <typename T>
-    void fillVector (std::vector<T>& vec) {
-        fillRange (vec.begin (), vec.end ());
-    }
-
-    /**
-     * \brief Fills the range [begin, end) with random values.
-     * \param[in] begin an iterator
-     * \param[in] end an iterator
-     *
-     * This supplies efficient implementation for raw pointer ranges, and std::vector iterators.
-     * For regular forward iterators this may be slow due to virtual call overhead.
-     */
-    template <typename Iter>
-    void fillRange (const Iter begin, const Iter end) {
-        fillRangeImpl_ (begin, end, boost::is_pointer<Iter>());
-    }
-
-private:
-
-    template <typename T>
-    inline void fillRangeImpl_ (T* const begin, T* const end,
-                                const boost::true_type& /* is_pointer */) {
-        return fillRangePointer_ (begin, end);
-    }
-
-    template <typename Iter>
-    inline void fillRangeImpl_ (const Iter begin, const Iter end,
-                                const boost::false_type& /* is_pointer */) {
-        typedef typename std::iterator_traits<Iter>::value_type value_type;
-        return fillRangeIterator_ (begin, end,
-                                   typename std::iterator_traits<Iter>::iterator_category (),
-                                   boost::is_same<Iter, typename std::vector<value_type>::iterator>());
-    }
-
-    /* Specialization for non-vector non-pointer iterator: */
-    template <typename Iter>
-    inline void fillRangeIterator_ (const Iter begin, const Iter end,
-                                    const std::forward_iterator_tag,
-                                    const boost::false_type& /* is_vector_iterator */)
-    {
-        typedef typename std::iterator_traits<Iter>::value_type value_type;
-        for (Iter i = begin; i != end; ++ i) {
-            *i = generateValue<value_type> ();
-        }
-    }
-
-    /* Specialization for vector iterator: */
-    template <typename Iter>
-    inline void fillRangeIterator_ (const Iter begin, const Iter end,
-                                    const std::random_access_iterator_tag,
-                                    const boost::true_type& /* is_vector_iterator */)
-    {
-        typedef typename std::iterator_traits<Iter>::difference_type diff_type;
-        typedef typename std::iterator_traits<Iter>::value_type value_type;
-        const diff_type num = end - begin;
-        if (num > 0) {
-            fillBytes (&*begin, sizeof (value_type) * num);
-        }
-    }
-
-    /* Specialization for non-void pointer iterator: */
-    template <typename T>
-    inline void fillRangePointer_ (T* const begin, T* const end) {
-        const ptrdiff_t num = end - begin;
-        if (num > 0) {
-            fillBytes (begin, sizeof (T) * num);
-        }
-    }
-
 }; /* class Random { */
-
-/* Specialization for void pointer iterator: */
-template <>
-inline void IRandom::fillRangePointer_<void> (void* const begin, void* const end) {
-    const ptrdiff_t num = static_cast<char*>(end) - static_cast<char*>(begin);
-    if (num > 0) {
-        fillBytes (begin, num);
-    }
-}
 
 
 } /* namespace sharemind { */
