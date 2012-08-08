964da61549619f0b6a6bb05e368dfb29d75f8689
Jaak Randmets <jaak.randmets@cyber.ee>
2012-08-08 16:19:42 +0300

    Add RNG to generate only zeroes.
    http://dilbert.com/strips/comic/2001-10-25/

---
+++ src/NullRandomEngine.cpp
@@ -0,0 +1,22 @@
+/*
+ * This file is a part of the Sharemind framework.
+ * Copyright (C) Cybernetica AS
+ *
+ * All rights are reserved. Reproduction in whole or part is prohibited
+ * without the written consent of the copyright owner. The usage of this
+ * code is subject to the appropriate license agreement.
+ */
+
+#include "NullRandomEngine.h"
+
+#include <cstring>
+
+namespace sharemind {
+
+void NullRandomEngine::Seed () { }
+
+void NullRandomEngine::fillBytes (void* memptr, size_t size) {
+    memset (memptr, 0, size);
+}
+
+} // namespace sharemind
---
+++ src/NullRandomEngine.h
@@ -0,0 +1,28 @@
+/*
+ * This file is a part of the Sharemind framework.
+ * Copyright (C) Cybernetica AS
+ *
+ * All rights are reserved. Reproduction in whole or part is prohibited
+ * without the written consent of the copyright owner. The usage of this
+ * code is subject to the appropriate license agreement.
+ */
+
+#ifndef SHAREMINDCOMMON_NULLRANDOMENGINE_H
+#define SHAREMINDCOMMON_NULLRANDOMENGINE_H
+
+#include "Random.h"
+
+namespace sharemind {
+
+/** A randomness engine that only generates zeroes. Useful for debugging. */
+class NullRandomEngine : public IRandom {
+public: /* Methods: */
+
+    virtual void Seed();
+
+    virtual void fillBytes (void* memptr, size_t size);
+};
+
+} /* namespace sharemind { */
+
+#endif /* SHAREMINDCOMMON_NULLRANDOMENGINE_H */
