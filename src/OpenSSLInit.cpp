0ed9c42fcd8e78574cf55e3e50babcb283561058
Jaak Ristioja <jaak.ristioja@cyber.ee>
2014-08-29 12:26:57 +0300

    Random, OpenSSLInit: Exception handling fix, small optimizations, code style changes.

--- src/OpenSSLInit.cpp
+++ src/OpenSSLInit.cpp
@@ -7,6 +7,7 @@
  * code is subject to the appropriate license agreement.
  */
 
+#include <cassert>
 #include <openssl/ssl.h>
 #include <openssl/crypto.h>
 #include <mutex>
@@ -20,32 +21,41 @@ namespace /* anonymous */ {
  * This is required to make OpenSSLRandomEngine thread safe.
  */
 class OpenSSLInit {
+
 private: /* Methods: */
 
-    OpenSSLInit () {
-        SSL_library_init ();
-
-        const size_t num_locks = CRYPTO_num_locks ();
-        m_mutexes.reserve (num_locks);
-        for (size_t i = 0; i < num_locks; ++ i) {
-            m_mutexes.emplace_back (new std::mutex);
+    OpenSSLInit() {
+        SSL_library_init();
+        if (size_t num_locks = CRYPTO_num_locks()) {
+            m_mutexes.reserve(num_locks);
+            do {
+                m_mutexes.emplace_back(new std::mutex);
+            } while (--num_locks);
+            CRYPTO_set_locking_callback(&openssl_locking_callback);
         }
-
-        CRYPTO_set_locking_callback (&openssl_locking_callback);
     }
 
-    static void openssl_locking_callback (int mode, int n, const char*, int) {
-        if (auto & mut = m_instance.m_mutexes.at (n)) {
-            if (mode & CRYPTO_LOCK)
-                mut->lock ();
-            else
-                mut->unlock ();
+    static void openssl_locking_callback(int mode,
+                                         int n,
+                                         const char *,
+                                         int) noexcept
+    {
+        assert(n >= 0);
+        assert(static_cast<unsigned>(n) < m_instance.m_mutexes.size());
+        if (const auto & mutex = m_instance.m_mutexes[n]) {
+            if (mode & CRYPTO_LOCK) {
+                mutex->lock();
+            } else {
+                mutex->unlock();
+            }
         }
     }
 
 private: /* Fields: */
+
     static OpenSSLInit                       m_instance;
     std::vector<std::unique_ptr<std::mutex>> m_mutexes;
+
 }; /* class OpenSSLInit { */
 
 OpenSSLInit OpenSSLInit::m_instance;
